11cd14034f631e033b7658bff59fd67ab9ef1d1b
Tanvir Rahman Nibir <tanvirrahmannibir2004@gmail.com>
2024-10-19 00:46:51 +0600

    Update random_password_generator.py

--- random_password_generator.py
+++ random_password_generator.py
@@ -12,16 +12,18 @@ password_list = []
 
 for char in range(1, nr_letters + 1):
   password_list.append(random.choice(letters))
-
+  print(password_list)
 for char in range(1, nr_symbols + 1):
-  password_list += random.choice(symbols)
+  password_list.append(random.choice(symbols))
+  print(password_list)
 
 for char in range(1, nr_numbers + 1):
-  password_list += random.choice(numbers)
+  password_list.append(random.choice(numbers))
+  print(password_list)
 
-print(password_list)
+# print(password_list)
 random.shuffle(password_list)
-print(password_list)
+# print(password_list)
 
 password = ""
 for char in password_list:
